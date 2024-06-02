9b48cf88b80da9bf1f5e208ac589cd15aee81936
thomneuenschwander <thom.nmaciel.baron@gmail.com>
2024-06-01 21:25:51 -0300

    LZW 12 bits

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 import database.algorithms.entropy.Entropy;
 import database.algorithms.entropy.huffman.HuffmanCompression;
+import database.algorithms.entropy.lzw.LZWCompression;
 import database.domain.FileType;
 
 public class Main {
     static final String COMPRESSION_SRC_PATH = "src\\database\\data\\data";
 
     static final String HUFFMAN_COMPRESSION_DST_PATH = "src\\database\\data\\entropy\\HUFFMAN_compressed";
+
     static final String HUFFMAN_DECOMPRESSION_SRC_PATH = HUFFMAN_COMPRESSION_DST_PATH;
     static final String HUFFMAN_DECOMPRESSION_DST_PATH = "src\\database\\data\\entropy\\HUFFMAN_decompressed";
 
     static final String LZW_COMPRESSION_DST_PATH = "src\\database\\data\\entropy\\LZW_compressed";
+
     static final String LZW_DECOMPRESSION_SRC_PATH = LZW_COMPRESSION_DST_PATH;
     static final String LZW_DECOMPRESSION_DST_PATH = "src\\database\\data\\entropy\\LZW_decompressed";
 
     public static void main(String[] args) throws Exception {
-
-        // HUFFMAN COMPRESSION AND DECOMPRESSION
 
         var huffmanEncoding = new Entropy()
                 .enableLogging(false)
@@ -31,21 +32,19 @@ public class Main {
                 .setDestine(HUFFMAN_DECOMPRESSION_DST_PATH)
                 .unzip(HuffmanCompression::decompress);
 
-        // LZW COMPRESSION AND DECOMPRESSION
-
         var lzwEncoding = new Entropy()
                 .enableLogging(false)
                 .setCompressionFileType(FileType.DB)
                 .setSource(COMPRESSION_SRC_PATH)
                 .setDestine(LZW_COMPRESSION_DST_PATH)
-                .zip(HuffmanCompression::compress);
+                .zip(LZWCompression::compress);
 
         var lzwDecoding = new Entropy()
                 .enableLogging(false)
                 .setCompressionFileType(FileType.DB)
                 .setSource(LZW_DECOMPRESSION_SRC_PATH)
                 .setDestine(LZW_DECOMPRESSION_DST_PATH)
-                .unzip(HuffmanCompression::decompress);
+                .unzip(LZWCompression::decompress);
 
         System.out.println("Huffman Compression Summary:");
         printSummary(huffmanEncoding);
@@ -61,16 +60,16 @@ public class Main {
     private static void printSummary(Entropy entropy) {
         System.out.println("Source File: " + entropy.getSource().getPath());
         System.out.println("Destination File: " + entropy.getDestine().getPath());
-        
-        long durationInMs = entropy.getDuration() / 1_000_000; 
+
+        long durationInMs = entropy.getDuration() / 1_000_000;
         System.out.println("Duration: " + durationInMs + " milliseconds");
-        
+
         double compressionPercentage = entropy.getCompressionPercentage();
-        if (compressionPercentage != 0) 
-            System.out.println("Compression Percentage: " + compressionPercentage + "%");
-        
+        if (compressionPercentage != 0)
+            System.out.println("Compression Percentage: " + String.format("%.2f", compressionPercentage) + "%");
+
         System.out.println("Entropy: " + entropy.getEntropy());
         System.out.println();
     }
-    
+
 }
--- src/database/algorithms/entropy/lzw/LZWCompression.java
+++ src/database/algorithms/entropy/lzw/LZWCompression.java
@@ -1,11 +1,8 @@
 package database.algorithms.entropy.lzw;
 
 import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -14,32 +11,48 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import database.algorithms.entropy.BitCompressor;
+
 public class LZWCompression {
+    public static void main(String[] args) {
+        try (var fis = new FileInputStream("source.db"); var fos = new FileOutputStream("destine.db")) {
+            LZWCompression.compress(fis, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try (var fis = new FileInputStream("destine.db"); var fos = new FileOutputStream("new_source.db")) {
+            LZWCompression.decompress(fis, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private static final int DICTIONARY_MAX_SIZE = 4095;
 
     private static List<Integer> generateLZWCodes(Map<List<Byte>, Integer> dictionary, byte[] bytes) {
         List<Integer> output = new LinkedList<>();
-        int dictSize = 256; 
+        int dictSize = 256;
         List<Byte> w = new LinkedList<>();
-        
+
         for (byte b : bytes) {
             List<Byte> wb = new LinkedList<>(w);
             wb.add(b);
 
-            if (dictionary.containsKey(wb)) 
+            if (dictionary.containsKey(wb))
                 w.add(b);
             else {
                 output.add(dictionary.get(w));
 
-                if (dictSize < DICTIONARY_MAX_SIZE) 
+                if (dictSize < DICTIONARY_MAX_SIZE)
                     dictionary.put(wb, dictSize++);
-                
+
                 w = new LinkedList<>();
                 w.add(b);
             }
         }
 
-        if (!w.isEmpty()) 
+        if (!w.isEmpty())
             output.add(dictionary.get(w));
 
         return output;
@@ -47,7 +60,7 @@ public class LZWCompression {
 
     private static Map<List<Byte>, Integer> initializeDictionary() {
         Map<List<Byte>, Integer> dictionary = new HashMap<>();
-        for (int i = 0; i < 256; i++) 
+        for (int i = 0; i < 256; i++)
             dictionary.put(Collections.singletonList((byte) i), i);
         return dictionary;
     }
@@ -60,13 +73,9 @@ public class LZWCompression {
 
             List<Integer> LZWCodes = generateLZWCodes(dictionary, fileBytes);
 
-            byte[] compressed = serializeLZWCodes(LZWCodes);
-
-            var dos = new DataOutputStream(dst);
-            dos.writeInt(LZWCodes.size()); 
-            dos.write(compressed); 
-
-            dos.close();
+            byte[] compressed = BitCompressor.compressTo12Bits(LZWCodes);
+            
+            dst.write(compressed);
             dst.close();
             src.close();
         } catch (Exception e) {
@@ -74,35 +83,15 @@ public class LZWCompression {
         }
     }
 
-    private static byte[] serializeLZWCodes(List<Integer> LZWCodes) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        DataOutputStream dos = new DataOutputStream(baos);
-
-        for (int code : LZWCodes)
-            dos.writeInt(code);
-
-        dos.flush();
-        return baos.toByteArray();
-    }
-
-    private static List<Integer> deserializeLZWCodes(DataInputStream dis, int size) throws IOException {
-        List<Integer> LZWCodes = new ArrayList<>(size);
-        for (int i = 0; i < size; i++) 
-            LZWCodes.add(dis.readInt());
-
-        return LZWCodes;
-    }
-
     public static void decompress(FileInputStream src, FileOutputStream dst) {
         try {
-            var dis = new DataInputStream(src);
-            int size = dis.readInt();
+            byte[] compressed = src.readAllBytes();
 
-            List<Integer> LZWCodes = deserializeLZWCodes(dis, size);
+            List<Integer> LZWCodes = BitCompressor.decompressFrom12Bits(compressed);
 
             List<byte[]> dictionary = new ArrayList<>();
-            for (int i = 0; i < 256; i++) 
-                dictionary.add(new byte[]{(byte) i});
+            for (int i = 0; i < 256; i++)
+                dictionary.add(new byte[] { (byte) i });
 
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
@@ -112,15 +101,14 @@ public class LZWCompression {
             for (int i = 1; i < LZWCodes.size(); i++) {
                 int currCode = LZWCodes.get(i);
                 byte[] entry;
-                if (currCode < dictionary.size()) 
+                if (currCode < dictionary.size())
                     entry = dictionary.get(currCode);
                 else if (currCode == dictionary.size()) {
                     byte[] prevEntry = dictionary.get(prevCode);
                     entry = Arrays.copyOf(prevEntry, prevEntry.length + 1);
                     entry[entry.length - 1] = prevEntry[0];
-                } else 
+                } else
                     throw new IllegalArgumentException("Invalid LZW code: " + currCode);
-                
 
                 baos.write(entry);
 
@@ -128,15 +116,14 @@ public class LZWCompression {
                 byte[] newEntry = Arrays.copyOf(prevEntry, prevEntry.length + 1);
                 newEntry[newEntry.length - 1] = entry[0];
 
-                if (dictionary.size() < DICTIONARY_MAX_SIZE) 
+                if (dictionary.size() < DICTIONARY_MAX_SIZE)
                     dictionary.add(newEntry);
-                
+
                 prevCode = currCode;
             }
 
             dst.write(baos.toByteArray());
 
-            dis.close();
             src.close();
             dst.close();
         } catch (Exception e) {
